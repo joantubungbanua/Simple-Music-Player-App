33c2cfe718d57325abfb9bfb6d96b193f8965944
Joan Marie Tubungbanua <jtubungbanua@Joans-MBP-4.home>
2020-05-05 07:34:19 -0400

    upload first music player app

---
+++ MusicPlayer/MusicPlayer/GenreButtonScreen.swift
@@ -0,0 +1,57 @@
+
+import UIKit
+import MediaPlayer
+
+class GenreButtonScreen: UIViewController {
+    
+    var musicPlayer = MPMusicPlayerController.applicationMusicPlayer
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    @IBAction func GenreButtonTapped(_ sender: UIButton) {
+        MPMediaLibrary.requestAuthorization { (status) in
+            if status == .authorized {
+                self.playGenre(genre: sender.currentTitle!)
+            }
+        }
+        
+    }
+    
+    @IBAction func StopButtonTapped(_ sender: UIButton) {
+        musicPlayer.stop()
+    }
+    
+    @IBAction func NextButtonTapped(_ sender: UIButton) {
+        musicPlayer.skipToNextItem()
+    }
+    
+    func playGenre(genre: String) {
+        musicPlayer.stop()
+        
+        let query = MPMediaQuery()
+        let predicate = MPMediaPropertyPredicate(value: genre, forProperty: MPMediaItemPropertyGenre)
+        
+        query.addFilterPredicate(predicate)
+        
+        
+        musicPlayer.setQueue(with:query)
+        musicPlayer.shuffleMode = .songs
+        musicPlayer.play()
+    }
+    
+ 
+    
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        
+    }
+    
+
+  
+}
